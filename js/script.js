ba58f965abe15edb53276169b787a81000e50fbf
Otabek <turayevotabek.0002@gmail.com>
2023-01-27 18:08:11 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 let cardNumber = "8600123456789101"
-console.log(cardNumber.slice(0, 4) + " ******** " + cardNumber.slice(12, 16))
+console.log(cardNumber.slice(0, 4) + " **** **** " + cardNumber.slice(12, 16))
 
 function checkCardNumber(card){
-    if(card == cardNumber){
-        console.log("It is right")
+    if(card.length == 16 && !Number.isNaN(+card)){
+        if(+card == +cardNumber){
+            console.log("It is right")
+        }else{
+            console.log("It is wrong. Try again")
+        }
     }else{
-        console.log("It is wrong. Try again")
+        console.log("Error")
     }
 }
+checkCardNumber(cardNumber)
 
 function checkCardtypeof(card){
-    if(card.slice(0, 4) == "8600"){
-        console.log("This is UzCard")
-    }else if(card.slice(0, 4) == "9860"){
-        console.log("This is Humo")
-    }
+    if(card.length == 16 && !Number.isNaN(+card)){
+        if(card.slice(0, 4) == "8600"){
+            console.log("This is UzCard")
+        }else if(card.slice(0, 4) == "9860"){
+            console.log("This is Humo")
+        }
+    }else{
+        console.log("Error")
+    }   
 }
-checkCardtypeof("9860123456789101")
-checkCardNumber("8600123456789101")
+checkCardtypeof(cardNumber)
